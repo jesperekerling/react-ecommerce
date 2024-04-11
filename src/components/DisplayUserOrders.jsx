0e9996d85df3be68d44d115aae46cfcb5ffd1214
Jesper Ekerling <jesper@forme.se>
2024-04-11 17:16:33 +0200

    My Orders print out change

--- src/components/DisplayUserOrders.jsx
+++ src/components/DisplayUserOrders.jsx
@@ -17,13 +17,13 @@ const DisplayUserOrders = () => {
       const ordersWithProducts = await Promise.all(
         response.data.map(async (order) => {
           const products = await Promise.all(
-            order.products.map(async (productId) => {
-              const productResponse = await axios.get(`https://ecommerce-api.ekerling.com/api/products/${productId}`, {
+            order.products.map(async (productItem) => {
+              const productResponse = await axios.get(`https://ecommerce-api.ekerling.com/api/products/${productItem.product.$oid}`, {
                 headers: {
                   Authorization: `Bearer ${token}`,
                 },
               });
-              return productResponse.data;
+              return { ...productResponse.data, quantity: productItem.quantity };
             })
           );
           return { ...order, products };
