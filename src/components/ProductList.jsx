3fa00ffdfebe978d3ec70b37ad520891a1359d94
Jesper Ekerling <jesper@forme.se>
2024-07-10 08:17:46 +0200

    PropType and displayName added

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,24 @@
-import { ProductCard } from "./ProductCard"
+
+import PropTypes from 'prop-types';
+import { ProductCard } from "./ProductCard";
 
 export const ProductList = ({ products }) => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      { products.map(product => (
+      {products.map(product => (
         <ProductCard key={product._id} product={product} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-ProductList.Skeleton = () => {
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+const ProductListSkeleton = () => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
@@ -24,7 +32,8 @@ ProductList.Skeleton = () => {
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
-      <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
     </div>
-  )
-}
+  );
+};
+
+ProductListSkeleton.displayName = 'ProductListSkeleton';
