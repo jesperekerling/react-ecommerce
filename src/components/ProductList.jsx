5e525d7a89cb9e00c9fd712fb7b4970e3b79ab7e
Jesper Ekerling <jesper@forme.se>
2024-07-10 08:23:49 +0200

    Changed back

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,24 +1,16 @@
-
-import PropTypes from 'prop-types';
-import { ProductCard } from "./ProductCard";
+import { ProductCard } from "./ProductCard"
 
 export const ProductList = ({ products }) => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      {products.map(product => (
+      { products.map(product => (
         <ProductCard key={product._id} product={product} />
       ))}
     </div>
-  );
-};
+  )
+}
 
-ProductList.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-  })).isRequired,
-};
-
-const ProductListSkeleton = () => {
+ProductList.Skeleton = () => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
@@ -32,8 +24,7 @@ const ProductListSkeleton = () => {
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
       <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
+      <div className="bg-gray-300/50 rounded-lg animate-pulse aspect-square" />
     </div>
-  );
-};
-
-ProductListSkeleton.displayName = 'ProductListSkeleton';
+  )
+}
