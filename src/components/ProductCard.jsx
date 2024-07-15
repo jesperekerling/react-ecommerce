fbd1a098898d6df7ea5286625a4e9a2ae814a43c
Jesper Ekerling <jesper@forme.se>
2024-07-15 23:24:09 +0200

    Color update on ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ export const ProductCard = ({ product }) => {
       <div className="p-4 items-center justify-between">
         <p className='font-bold text-sm md:text-base hover:text-gray-500'>{product.name}</p>
         <p className='text-gray-600 dark:text-gray-300 text-xs my-2 md:text-sm'>{product.category}</p>
-        <p className='text-gray-700 dark:text-blue-300 font-semibold text-xs md:text-sm'>
+        <p className='text-gray-700 dark:text-gray-200 font-semibold text-xs md:text-sm'>
           {new Intl.NumberFormat('sv-SE', { style: 'currency', currency: 'SEK' }).format(product.price)}
         </p>
       </div>
