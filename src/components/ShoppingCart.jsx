72b5b9a57d7b8daa47022ce7793668a081e85672
Jesper Ekerling <jesper@forme.se>
2024-08-22 22:58:44 +0200

    ShoppingCart.jsx back again..

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -4,7 +4,6 @@ import { Link, NavLink } from "react-router-dom"
 import { useCart } from "../contexts/cartContext"
 import { useAuth } from '../contexts/authContext';
 import { toast } from 'react-toastify';
-import { ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
 export const ShoppingCart = ({ isCheckOutPage, setIsOpen }) => {
@@ -14,37 +13,47 @@ export const ShoppingCart = ({ isCheckOutPage, setIsOpen }) => {
   const { cart, totalPrice, clearCart } = useCart()
 
   const placeOrder = async () => {
-    const response = await fetch('https://js2-ecommerce-api.vercel.app/api/orders', {
+    const orderProducts = cart.map(item => ({
+      productId: item.product._id,
+      quantity: item.quantity
+    }));
+  
+    // Log the product IDs and quantities
+    console.log('Order products:', orderProducts);
+  
+    const response = await fetch('https://ecommerce-api.ekerling.com/api/orders', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       },
-      body: JSON.stringify({ ...orderData, userId })
+      body: JSON.stringify({
+        products: orderProducts
+      })
     });
-
+  
     if (!response.ok) {
-      <p>Errorrrrrr!!!</p>
+      console.error('Error placing order:', response.statusText);
+      return;
     }
-
+  
     // Displays answer from API in the console
     const responseData = await response.json();
     console.log(responseData);
-
+  
     toast.success("Thank you, your order has been placed!");
-
+  
     clearCart();
-
+  
     setTimeout(() => {
       navigate('/logged-in', { state: { from: 'placeOrder' } });
     }, 1000);
-
   };
 
     return (
-    <div className="bg-white py-5 px-8">
-      <ToastContainer />
-      <div>
+    <div className="bg-white dark:bg-gray-950 text-black dark:text-white py-2 sm:py-3 px-3 sm:px-8">
+      
+      <div className="mb-5">
         { cart.length < 1 && (
           <div className="p-2 text-center">
             <p>Your cart is empty..</p>
@@ -59,16 +68,16 @@ export const ShoppingCart = ({ isCheckOutPage, setIsOpen }) => {
           <CartItem key={`cart_${item.product._id}`} item={item} />
         ))}
       </div>
-      <hr className="border-gray-400" />
+      <hr className="border-gray-400 dark:border-e-gray-700" />
       <div className="flex justify-between items-center p-2 mt-3">
         <div>
-          <p className="text-black">Total Price: <span className="font-bold">{ totalPrice } kr</span></p>
-          <small className="text-gray-600">Inkl. vat</small>
+          <p className="text-black dark:text-white">Total Price: <span className="font-bold">{new Intl.NumberFormat('sv-SE').format(totalPrice)} kr</span></p>
+          <small className="text-gray-600 dark:text-gray-200">Inkl. vat</small>
         </div>
         <div>
           { isCheckOutPage && token && (
             <>
-              <button onClick={clearCart} className="bg-gray-200 text-black py-1.5 px-6 rounded-lg transition-colors mr-4">
+              <button onClick={clearCart} className="bg-gray-200 dark:bg-gray-800s text-black dark:text-white dark:bg-slate-800 py-1.5 px-6 rounded-lg transition-colors mr-4">
                 Clear cart
               </button>
               <button onClick={placeOrder} className="bg-blue-700 text-white py-1.5 px-6 rounded-lg hover:bg-slate-900 transition-colors">
@@ -77,7 +86,7 @@ export const ShoppingCart = ({ isCheckOutPage, setIsOpen }) => {
             </>
           )}
           { !isCheckOutPage && (
-            <Link onClick={() => setIsOpen(false)} to="/checkout" className="bg-blue-700 text-white py-3 px-6 rounded-lg hover:bg-blue-600 hover:text-white">
+            <Link onClick={() => setIsOpen(false)} to="/checkout" className="bg-blue-700 font-semibold text-white py-3 px-6 rounded-lg hover:bg-blue-600 hover:text-white">
               Checkout
             </Link>
           )}
@@ -85,7 +94,7 @@ export const ShoppingCart = ({ isCheckOutPage, setIsOpen }) => {
       </div>
       
       {!token && (
-        <p className="p-5 bg-blue-100 mt-5 rounded-lg">
+        <p className="p-5 bg-blue-100 dark:bg-gray-900 mt-5 rounded-lg">
           Please <NavLink to="/register" className="font-bold">register</NavLink> or <NavLink to="/login" className="font-bold">log in</NavLink> to proceed with your checkout.
         </p>
       )}
